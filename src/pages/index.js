cd8f9e5eb98e44f5bfa2e997a88ac288c05d5801
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-22 00:50:05 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,33 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { getSession } from "next-auth/react";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const { data } = getSession();
-    if (!data) {
-      router.replace("/DashBoard");
-    }
-  }, [router]);
-
   return <></>;
 }
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/DashBoard",
+        permanent: false,
+      },
+    };
+  }
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signup",
+        permanent: false,
+      },
+    };
+  }
+  
+  return {
+    props: {
+      session,
+    },
+  };
+}
+
