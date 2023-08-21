d4a78b87ed7de5963142bda2931f525ac413a852
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-22 00:49:28 +0500

    Update index.js

--- src/components/Headings/index.js
+++ src/components/Headings/index.js
@@ -1,9 +1,13 @@
 import { Layout } from "antd";
+import Head from "next/head";
 const { Header } = Layout;
 
 const Headings = ({headingName}) => {
   return (
     <>
+    <Head >
+      <title>{headingName}</title>
+    </Head>
       <Header
         style={{
           backgroundColor: "##102e4b",
