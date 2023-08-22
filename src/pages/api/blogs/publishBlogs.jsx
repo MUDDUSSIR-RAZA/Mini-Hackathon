f53f3a989209f2bbe5e33eaf83081a1a676ee70a
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-22 15:54:22 +0500

    Update publishBlogs.jsx

--- src/pages/api/blogs/publishBlogs.jsx
+++ src/pages/api/blogs/publishBlogs.jsx
@@ -2,7 +2,7 @@ import { publishBlog } from "@/services/blogs";
 
 export default async function handler(req, res) {
   const { title, description, dateTime, email } = req.body;
-  console.log(req.method);
+  
   if (req.method === "POST") {
     publishBlog(title, description, dateTime, email);
     return res.status(200).send();
