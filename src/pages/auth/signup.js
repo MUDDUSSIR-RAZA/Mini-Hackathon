be550ee1eed331dc527f7a129f8e0c30ba534a52
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-22 17:22:31 +0500

    Update signup.js

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -145,7 +145,6 @@ export default function signup() {
                   type="password"
                   ref={passwordRef}
                   placeholder="Password"
-                  autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 py-2"
                 />
@@ -158,7 +157,6 @@ export default function signup() {
                   type="password"
                   ref={repeatPasswordRef}
                   placeholder="Repeat Password"
-                  autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 py-2"
                 />
