77ea0f33f4647d13a497f6fa79cba55da188b893
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-22 17:22:33 +0500

    Update login.js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -88,7 +88,6 @@ export default function Form(email, password) {
                   type="password"
                   ref={passwordRef}
                   placeholder="Password"
-                  autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 py-2"
                 />
